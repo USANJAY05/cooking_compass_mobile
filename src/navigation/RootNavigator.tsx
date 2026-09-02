439826714e6a0714f7e5359686fbb68def0abac2
SANJAY U <115535475+USANJAY05@users.noreply.github.com>
2026-09-02 11:36:13 +0530

    feat: add preferences settings route

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -11,6 +11,7 @@ import { RoutineDetailScreen } from '../screens/RoutineDetailScreen';
 import { EditRoutineScreen } from '../screens/EditRoutineScreen';
 import { NutritionDetailScreen } from '../screens/NutritionDetailScreen';
 import { AppearanceSettingsScreen } from '../screens/AppearanceSettingsScreen';
+import { PreferencesSettingsScreen } from '../screens/PreferencesSettingsScreen';
 import { AccountSettingsScreen } from '../screens/AccountSettingsScreen';
 import { SecuritySettingsScreen } from '../screens/SecuritySettingsScreen';
 import { InteractiveCookingSettingsScreen } from '../screens/InteractiveCookingSettingsScreen';
@@ -29,8 +30,8 @@ export const RootNavigator = () => {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-        screenOptions={{ 
+      <Stack.Navigator
+        screenOptions={{
           headerShown: false,
           headerStyle: { backgroundColor: theme.colors.surface },
           headerTintColor: theme.colors.text,
@@ -40,112 +41,22 @@ export const RootNavigator = () => {
         {user ? (
           <>
             <Stack.Screen name="MainTabs" component={BottomTabs} />
-            <Stack.Screen 
-              name="CreateRecipe" 
-              component={CreateRecipeScreen} 
-              options={{
-                headerShown: true,
-                title: 'Create Recipe',
-              }}
-            />
-            <Stack.Screen 
-              name="RecipeDetail" 
-              component={RecipeDetailScreen} 
-              options={{
-                headerShown: true,
-                title: 'Recipe Detail',
-              }}
-            />
-            <Stack.Screen 
-              name="EditRecipe" 
-              component={EditRecipeScreen} 
-              options={{
-                headerShown: true,
-                title: 'Edit Recipe',
-              }}
-            />
-            <Stack.Screen 
-              name="CreateRoutine" 
-              component={CreateRoutineScreen} 
-              options={{
-                headerShown: true,
-                title: 'Create Routine',
-              }}
-            />
-            <Stack.Screen 
-              name="RoutineDetail" 
-              component={RoutineDetailScreen} 
-              options={{
-                headerShown: true,
-                title: 'Routine Detail',
-              }}
-            />
-            <Stack.Screen 
-              name="EditRoutine" 
-              component={EditRoutineScreen} 
-              options={{
-                headerShown: true,
-                title: 'Edit Routine',
-              }}
-            />
-            <Stack.Screen
-              name="NutritionDetail"
-              component={NutritionDetailScreen}
-              options={{
-                headerShown: true,
-                title: 'Full Nutrition',
-              }}
-            />
-            <Stack.Screen
-              name="AppearanceSettings"
-              component={AppearanceSettingsScreen}
-              options={{ headerShown: true, title: 'Appearance' }}
-            />
-            <Stack.Screen
-              name="AccountSettings"
-              component={AccountSettingsScreen}
-              options={{ headerShown: true, title: 'Account information' }}
-            />
-            <Stack.Screen
-              name="SecuritySettings"
-              component={SecuritySettingsScreen}
-              options={{ headerShown: true, title: 'Security' }}
-            />
-            <Stack.Screen
-              name="About"
-              component={AboutScreen}
-              options={{
-                headerShown: true,
-                title: 'About',
-              }}
-            />
-            <Stack.Screen
-              name="PrivacyPolicy"
-              component={PrivacyPolicyScreen}
-              options={{
-                headerShown: true,
-                title: 'Privacy Policy',
-              }}
-            />
-
-            <Stack.Screen
-              name="Terms"
-              component={TermsScreen}
-              options={{
-                headerShown: true,
-                title: 'Terms of Service',
-              }}
-            />
-            <Stack.Screen
-              name="RecipeCreationSettings"
-              component={RecipeCreationSettingsScreen}
-              options={{ headerShown: true, title: 'Recipe creation' }}
-            />
-            <Stack.Screen
-              name="InteractiveCookingSettings"
-              component={InteractiveCookingSettingsScreen}
-              options={{ headerShown: true, title: 'Interactive cooking' }}
-            />
+            <Stack.Screen name="CreateRecipe" component={CreateRecipeScreen} options={{ headerShown: true, title: 'Create Recipe' }} />
+            <Stack.Screen name="RecipeDetail" component={RecipeDetailScreen} options={{ headerShown: true, title: 'Recipe Detail' }} />
+            <Stack.Screen name="EditRecipe" component={EditRecipeScreen} options={{ headerShown: true, title: 'Edit Recipe' }} />
+            <Stack.Screen name="CreateRoutine" component={CreateRoutineScreen} options={{ headerShown: true, title: 'Create Routine' }} />
+            <Stack.Screen name="RoutineDetail" component={RoutineDetailScreen} options={{ headerShown: true, title: 'Routine Detail' }} />
+            <Stack.Screen name="EditRoutine" component={EditRoutineScreen} options={{ headerShown: true, title: 'Edit Routine' }} />
+            <Stack.Screen name="NutritionDetail" component={NutritionDetailScreen} options={{ headerShown: true, title: 'Full Nutrition' }} />
+            <Stack.Screen name="PreferencesSettings" component={PreferencesSettingsScreen} options={{ headerShown: true, title: 'Preferences' }} />
+            <Stack.Screen name="AppearanceSettings" component={AppearanceSettingsScreen} options={{ headerShown: true, title: 'Appearance' }} />
+            <Stack.Screen name="AccountSettings" component={AccountSettingsScreen} options={{ headerShown: true, title: 'Account information' }} />
+            <Stack.Screen name="SecuritySettings" component={SecuritySettingsScreen} options={{ headerShown: true, title: 'Security' }} />
+            <Stack.Screen name="About" component={AboutScreen} options={{ headerShown: true, title: 'About' }} />
+            <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicyScreen} options={{ headerShown: true, title: 'Privacy Policy' }} />
+            <Stack.Screen name="Terms" component={TermsScreen} options={{ headerShown: true, title: 'Terms of Service' }} />
+            <Stack.Screen name="RecipeCreationSettings" component={RecipeCreationSettingsScreen} options={{ headerShown: true, title: 'Recipe creation' }} />
+            <Stack.Screen name="InteractiveCookingSettings" component={InteractiveCookingSettingsScreen} options={{ headerShown: true, title: 'Interactive cooking' }} />
           </>
         ) : (
           <Stack.Screen name="Login" component={LoginScreen} />
